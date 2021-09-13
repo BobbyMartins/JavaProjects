7d48120649bb9e5abb7d68b4a9a9ae4e54ac9857
Bobby Martins <57994611+BobbyMartins@users.noreply.github.com>
2021-09-13 23:31:42 +0100

    Fun Catch game using A* pathfinding algorithm

---
+++ src/node.java
@@ -0,0 +1,11 @@
+public class node {
+    // open/closed states
+    public boolean isOpen;
+    public boolean isClosed;
+    // position of parent
+    public int parentx,parenty;
+    // A* data
+    public int f,g,h;
+    // position on map
+    public int x,y;
+}
